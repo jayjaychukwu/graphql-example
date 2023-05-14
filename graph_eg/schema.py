3ae12d6c1b29e8e495660f253da0eb49adb866cf
jayjaychukwu <liljaydon.pj@gmail.com>
2023-05-15 00:21:48 +0100

    concluded with Auth

--- graph_eg/schema.py
+++ graph_eg/schema.py
@@ -1,7 +1,10 @@
 import graphene
+import graphql_jwt
 from graphene_django import (
     DjangoObjectType,  # this import is used to change Django objects to F
 )
+from graphql_auth import mutations
+from graphql_auth.schema import MeQuery, UserQuery
 
 from main_app.models import Contact
 
@@ -78,7 +81,22 @@ class ContactDelete(graphene.Mutation):
         contact.delete()
 
 
-class Query(graphene.ObjectType):
+class AuthMutation(graphene.ObjectType):
+    """For the registration, verification and logging in of users"""
+
+    register = mutations.Register.Field()  # predefined settings to register user
+    token_auth = mutations.ObtainJSONWebToken.Field()  # used to verify account
+    verify_account = mutations.VerifyAccount.Field()  # get the JWT to log in
+
+    # # For handling JWTS
+    # refresh_token = graphql_jwt.Refresh.Field()
+
+
+class Query(
+    UserQuery,
+    MeQuery,
+    graphene.ObjectType,
+):  # added UserQuery and MeQuery for the purpose of logging in, registering and verifying users
     # query ContactType class to get the list of contacts
     list_contact = graphene.List(ContactType)  # list all the Contact objects
     read_contact = graphene.Field(
@@ -95,7 +113,7 @@ class Query(graphene.ObjectType):
         return Contact.objects.get(id=id)
 
 
-class Mutation(graphene.ObjectType):
+class Mutation(AuthMutation, graphene.ObjectType):  # added AuthMutation of Users purpose
     # keywords that will be used to do the mutation in the frontend
     create_contact = ContactMutation.Field()
     update_contact = ContactMutation.Field()
