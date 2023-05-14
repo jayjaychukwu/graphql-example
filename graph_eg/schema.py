b61b62ad242ce3c1c6c814224c9c84e1bba86ae7
jayjaychukwu <liljaydon.pj@gmail.com>
2023-05-14 12:54:19 +0100

    update fields

--- graph_eg/schema.py
+++ graph_eg/schema.py
@@ -17,25 +17,37 @@ class ContactMutation(graphene.Mutation):
     class Arguments:
         # add fields you would like to create.
         # this will corelate with the ContactType fields above
+        id = graphene.ID()  # this is for updating purpose
         name = graphene.String()
         phone_number = graphene.String()
 
     contact = graphene.Field(ContactType)  # define the class we are the fields from
 
     @classmethod
-    def mutate(cls, root, info, name, phone_number):
+    def mutate(cls, root, info, name, phone_number, id):
         # function that will save the data to the db
+        #######CREATE###########
         contact = Contact(
             name=name,
             phone_number=phone_number,
         )  # accepts all fields
         contact.save()  # save the contact to the database
 
+        ##########UPDATE########################
+        get_contact = Contact.objects.get(id=id)
+        get_contact.name = name  # assign a new name
+        get_contact.phone_number = phone_number  # assign a new phone_number
+        get_contact.save()
+        return ContactMutation(contact=get_contact)
+
 
 class Query(graphene.ObjectType):
     # query ContactType class to get the list of contacts
-    list_contact = graphene.List(ContactType)
-    read_contact = graphene.Field(ContactType, id=graphene.Int())  # id = graphene.Int
+    list_contact = graphene.List(ContactType)  # list all the Contact objects
+    read_contact = graphene.Field(
+        ContactType,
+        id=graphene.Int(),
+    )  # id = graphene.Int, retrieve a particular contact object
 
     def resolve_list_contact(root, info):
         # can easily optimize query count in the resolve method
@@ -49,6 +61,7 @@ class Query(graphene.ObjectType):
 class Mutation(graphene.ObjectType):
     # keywords that will be used to do the mutation in the frontend
     create_contact = ContactMutation.Field()
+    update_contact = ContactMutation.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
